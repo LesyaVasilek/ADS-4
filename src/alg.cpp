079ef5041d1cd44c803d71bf5f5cc872d19dd32a
LesyaVasilek <125215125+LesyaVasilek@users.noreply.github.com>
2023-03-26 21:38:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,73 +1,55 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  int search = 0;
+int countPairs1(int* arr, int len, int value) {
+    int search = 0;
     for (int i = 0; i < len; i++) {
         for (int j = i + 1; j < len; j++) {
-            if (arr[i] + arr[j] == value) {
+            if (*(arr + i) + *(arr + j) == value) {
                 search++;
             }
         }
     }
     return search;
 }
-int countPairs2(int *arr, int len, int value) {
+int countPairs2(int* arr, int len, int value) {
+    while (*(arr + (len - 1)) > value) len--;
     int search = 0;
-    int r = len - 1;
-    int l = 0;
-    while (l < r) {
-        int sum = arr[l] + arr[r];
-        if (sum == value) {
-            search++;
-            while (l < r && arr[l] == arr[l+1]) {
-                l++;
+    for (int i = 0; i < len; i++) {
+        for (int j = len; j > i; j--) {
+            if (*(arr + i) + *(arr + j) == value){
+              search++;
             }
-            while (l < r && arr[r] == arr[r-1]) {
-                r--;
-            }
-            l++;
-            r--;
-        } else if (sum < value) {
-            l++;
-        } else {
-            r--;
         }
     }
     return search;
 }
-int binarysearch(int *arr, int len, int value)
-{
-    int lol = 0;
-    int ryka = len - 1;
-    while (lol <= ryka)
-    {
-        int midble = lol + (ryka - lol) / 2;
-        if (arr[midble] == value)
-        {
-            return midble;
+int binerySearch(int* arr, int size, int value, int n) {
+    int ror = size;
+    int lol = n;
+    int search = 0;
+    while (lol < ror - 1) {
+        int sror = (lol + ror) / 2;
+        if (*(arr + n)  + *(arr + sror) == value) {
+            search++;
+            int i = 1;
+            for (; *(arr + n) + *(arr + sror - i) == value && (sror - i) > lol; i++)
+                search++;
+            i = 1;
+            for (; *(arr + n) + *(arr + sror + i) == value && (sror + i) < ror; i++)
+                search++;
+            break;
         }
-        else if (arr[midble] < value)
-        {
-            lol = midble + 1;
-        }
-        else
-        {
-            ryka = midble - 1;
+        if (*(arr + n) + *(arr + sror) > value) {
+            ror = sror;
+        } else {
+            lol = sror;
         }
     }
-    return -1;
+    return search;
 }
-
-int countPairs3(int *arr, int len, int value)
-{
+int countPairs3(int* arr, int len, int value) {
     int search = 0;
-    for (int i = 0; i < len - 1; i++)
-    {
-        int brain = value - arr[i];
-        int idfcare = binarysearch(arr + i + 1, len - i - 1, brain);
-        if (idfcare != -1)
-        {
-            search++;
-        }
+    for (int i = 0; i < len - 1; i++) {
+        search += binerySearch(arr, len, value, i);
     }
     return search;
 }
